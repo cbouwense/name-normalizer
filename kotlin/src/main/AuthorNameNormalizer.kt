3984811123edf2e35b1f1df9a9864e269db59153
Christian Bouwense <bouwensec@gmail.com>
2022-10-29 17:36:09 -0400

    test 5 green

--- kotlin/src/main/AuthorNameNormalizer.kt
+++ kotlin/src/main/AuthorNameNormalizer.kt
@@ -6,8 +6,18 @@ class AuthorNameNormalizer() {
 
         val tokens = trimmedName.split(" ")
 
+        if (tokens.size == 3) return normalizeNameWithMiddleInitial(tokens)
         if (tokens.size == 2) return swapFirstAndLastNames(tokens)
         return trimmedName
+    }
+
+    private fun normalizeNameWithMiddleInitial(tokens: List<String>): String {
+        return buildString {
+            append(swapFirstAndLastNames(listOf(tokens[0], tokens[2])))
+            append(" ")
+            append(tokens[1].first())
+            append(".")
+        }
     }
 
     private fun swapFirstAndLastNames(tokens: List<String>): String {
