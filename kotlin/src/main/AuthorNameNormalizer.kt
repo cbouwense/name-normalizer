1b5ecdd526d0f12207cf9cd81bb9e47c23b1b686
Christian Bouwense <bouwensec@gmail.com>
2022-10-29 17:20:11 -0400

    test four green

--- kotlin/src/main/AuthorNameNormalizer.kt
+++ kotlin/src/main/AuthorNameNormalizer.kt
@@ -10,13 +10,21 @@ class AuthorNameNormalizer() {
     fun normalize(name: String): String {
         if (name == "") return ""
 
-        val tokens = name.split(" ")
+        val trimmedName = trimWhitespace(name)
+
+        val tokens = trimmedName.split(" ")
 
         if (tokens.size == 2) return swapFirstAndLastNames(tokens)
-        return name
+        return trimmedName
     }
 
     private fun swapFirstAndLastNames(tokens: List<String>): String {
         return tokens[1] + ", " + tokens[0]
+    }
+
+    private fun trimWhitespace(name: String): String {
+        val trimmed = name.trim(' ')
+        println("trimmed: $trimmed")
+        return trimmed
     }
 }
