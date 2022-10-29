187d7f12a981044adb98ab3ce3fc133c16cdf7b7
Christian Bouwense <bouwensec@gmail.com>
2022-10-29 17:11:42 -0400

    third test green

--- kotlin/src/main/AuthorNameNormalizer.kt
+++ kotlin/src/main/AuthorNameNormalizer.kt
@@ -6,13 +6,16 @@
 
 
 
-
-
-
-
 class AuthorNameNormalizer() {
     fun normalize(name: String): String {
         if (name == "") return ""
-        return "Plato"
+        
+        val tokens = name.split(" ")
+
+        if (tokens.size == 1) return name
+
+        val reversedName = tokens[1] + ", " + tokens[0]
+
+        return reversedName
     }
 }
